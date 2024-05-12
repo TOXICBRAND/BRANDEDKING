05794d4dd87de7efd3ec3de5887d9e5e53623446
TGMBBRAND <laskhmanrao000@gmail.com>
2024-05-12 18:24:47 +0530

    Update userbot.py

--- BRANDEDKING/core/userbot.py
+++ BRANDEDKING/core/userbot.py
@@ -51,9 +51,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDRD_BOT")
-                await self.one.join_chat("BRANDED_PAID_CC")
+                await self.one.join_chat("TEL_EGRAM_WALI_MAST")
+                await self.one.join_chat("MR_ZEHER_TAJ")
+                await self.one.join_chat("LOVE_YOU_ZINDAGI3")
             except:
                 pass
             assistants.append(1)
@@ -73,9 +73,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("BRANDED_WORLD")
-                await self.two.join_chat("BRANDRD_BOT")
-                await self.one.join_chat("BRANDED_PAID_CC")
+                await self.two.join_chat("TEL_EGRAM_WALI_MAST")
+                await self.two.join_chat("MR_ZEHER_TAJ")
+                await self.one.join_chat("LOVE_YOU_ZINDAGI3")
             except:
                 pass
             assistants.append(2)
@@ -95,9 +95,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("BRANDED_WORLD")
-                await self.three.join_chat("BRANDRD_BOT")
-                await self.one.join_chat("BRANDED_PAID_CC")
+                await self.three.join_chat("TEL_EGRAM_WALI_MAST")
+                await self.three.join_chat("MR_ZEHER_TAJ")
+                await self.one.join_chat("LOVE_YOU_ZINDAGI3")
             except:
                 pass
             assistants.append(3)
@@ -117,9 +117,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("BRANDED_WORLD")
-                await self.four.join_chat("BRANDRD_BOT")
-                await self.one.join_chat("BRANDED_PAID_CC")
+                await self.four.join_chat("TEL_EGRAM_WALI_MAST")
+                await self.four.join_chat("MR_ZEHER_TAJ")
+                await self.one.join_chat("LOVE_YOU_ZINDAGI3")
             except:
                 pass
             assistants.append(4)
@@ -139,9 +139,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("BRANDED_WORLD")
-                await self.five.join_chat("BRANDRD_BOT")
-                await self.one.join_chat("BRANDED_PAID_CC")
+                await self.five.join_chat("TEL_EGRAM_WALI_MAST")
+                await self.five.join_chat("MR_ZEHER_TAJ")
+                await self.one.join_chat("LOVE_YOU_ZINDAGI3")
             except:
                 pass
             assistants.append(5)
