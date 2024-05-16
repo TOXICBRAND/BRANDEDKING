b9f816f13916e443cd063eba094e4f357aab4f51
TGMBBRAND <laskhmanrao000@gmail.com>
2024-05-16 22:14:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+OUGD523K--8yOWRl")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+OUGD523K--8yOWRl")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MID_NIGHT_WORLD")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MID_NIGHT_WORLD")
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", None)
@@ -83,13 +83,13 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/c49df2081c630ea5b2fa2.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/afaf9fe60aec47093a00a.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph/file/83ba36fcaa53f1c39cbd7.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/c49df2081c630ea5b2fa2.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/afaf9fe60aec47093a00a.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
